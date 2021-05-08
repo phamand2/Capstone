45008724bcfaf1fec429fff712a6fbfed8f94613
naldurien <nicole.aldurien@gmail.com>
2021-05-08 00:03:17 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5001
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
