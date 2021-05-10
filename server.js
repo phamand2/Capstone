4221da01b1e616990d674ee8a15adab759cb172f
naldurien <nicole.aldurien@gmail.com>
2021-05-09 20:52:08 -0400

    sketching out auth routes; can add customers to db through postman

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5001
+const connectDB = require('./config/db')
+
+// Connect DB
+connectDB()
+
+
+// Middleware
+app.use(express.json())
+app.use('/api/auth', require('./routes/authRoutes'))
 
 
 
 
 
-
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
+})
+
+// makes giant server errors concise and simple to read
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Logged Error: ${err}`)
+  server.close(() => process.exit(1))
 })
