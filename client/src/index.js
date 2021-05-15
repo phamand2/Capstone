8de57d3d33b07d33bb753827527961fa1b8a8b2d
Neil Shah <77591107+neilshah101@users.noreply.github.com>
2021-05-14 20:07:01 -0500

    update in name of Baselayout to BaseLayout

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import ProductCategoryFlowers from './components/pages/ProductCategoryFlowers'
 import ProductDetails from './components/pages/ProductDetails'
 import MyCart from './components/pages/MyCart'
 import ProductManage from '../src/components/pages/ProductManage'
-import BaseLayout from './components/pages/Baselayout';
+import BaseLayout from './components/pages/BaseLayout';
 import CustRegisterPage from './components/pages/CustRegisterPage'
 import AddStaffPage from './components/pages/AddStaffPage'
 import AddAdminPage from './components/pages/AddAdminPage'
