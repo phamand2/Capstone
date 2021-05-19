55fff8fa5149b9b80a84202bf1192ea839e38894
Andrew Pham <andrewpham92@gmail.com>
2021-05-19 13:07:25 -0400

    fix

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    await mongoose.connect('process.env.MONGO_URI', {
+    await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true, 
         useFindAndModify: true,
         useCreateIndex: true,
