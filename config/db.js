a49a7b99bce243a2f3308bc730263db79d7a3471
Andrew Pham <andrewpham92@gmail.com>
2021-05-19 12:52:27 -0400

    url update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect('mongodb+srv://devcapstone:devcapstone@cluster0.ne7qw.mongodb.net/storedb?retryWrites=true&w=majority', {
         useNewUrlParser: true, 
         useFindAndModify: true,
         useCreateIndex: true,
